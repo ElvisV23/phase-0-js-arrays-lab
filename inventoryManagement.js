671a00c1400fbe8176af44b21a6609ce22d406b2
Elvis Valenzuela <Elvis.valenzuela23@gmail.com>
2025-02-20 01:32:40 -0500

    Array Lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,36 @@
 // Write your code here
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+console.log(products);
 
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+function addProduct(productName) {
+  products.push(productName);
+  console.log(products);
+}
+
+function updateProductName(position, newName) {
+  if (position >= 0 && position < products.length) {
+    console.log(products[position]);
+    products[position] = newName;
+    console.log(position,products[position]);
+  } else {
+    console.log("Invalid position. Please provide a valid index.");
+  }
+}
+
+function removeLastProduct() {
+  if (products.length > 0) {
+    const removedProduct = products.pop();
+    console.log(removedProduct);
+    console.log(products);
+  } else {
+    console.log("The products array is already empty.");
+  }
+}
 
 // Export the necessary parts for testing
 module.exports = {
